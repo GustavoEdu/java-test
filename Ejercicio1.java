4934313021e483f2d7b09cb63ed60425bcdd6a87
GustavoEdu10111213 <88157777+GustavoEdu@users.noreply.github.com>
2021-11-19 23:27:18 -0500

    Construí la función esPrimo para determinar si el número ingresado es primo o no y otra función que transforma el valor booleano de la primera función a una respuesta de String.

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -5,6 +5,29 @@ public class Ejercicio1 {
     System.out.print("Introduzca una numero: ");
     int num = sc.nextInt();
     System.out.println("El factorial de dicho numero es: " + factorial(num));
+    System.out.println("El numero es primo?: " + mostrarConsulta(esPrimo(num)));
+  }
+  public static String mostrarConsulta(boolean result) {
+    if(result) {
+      return "Si";
+    }
+    return "No";
+  }
+  public static boolean esPrimo(int num) {
+    if(num == 1) {
+      return false;
+    }
+    else if(num == 2) {
+      return true;
+    }
+    else {
+      for(int i = 3; i < num; i++) {
+        if(num % i == 0) {
+          return false;
+        }
+      }
+      return true;
+    }
   }
   public static int factorial(int num) {
     if(num == 0 || num == 1) {
